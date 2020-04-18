8aa828c082dd504bc5ee2555af4f2b2bcca88a51
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-04-18 19:01:56 +0300

    structure,routes,hbs

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({
+  extended: true
+}))
+
+//Routes
+app.use('/', require('./routes/home'))
+app.use('/courses', require('./routes/courses'))
+app.use('/add', require('./routes/add'))
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
