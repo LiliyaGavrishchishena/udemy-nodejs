63d70cbe024029a961f7b85c841b80788e1793d1
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-04-24 07:30:09 +0300

    cart functionality

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(node => {
-  node.textContent = new Intl.NumberFormat('uk-UK', {
+const toCurrency = price => {
+  return new Intl.NumberFormat('uk-UK', {
     currency: 'uah',
     style: 'currency'
-  }).format(node.textContent)
+  }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+  node.textContent = toCurrency(node.textContent)
 })
 
 const $cart = document.querySelector('#cart');
@@ -11,11 +15,26 @@ if ($cart) {
     if (event.target.classList.contains('js-remove')) {
       const id = event.target.dataset.id;
 
-      fetch('cart/remove' + id, {
+      fetch('/cart/remove/' + id, {
           method: "delete"
         }).then(res => res.json())
         .then(cart => {
-
+          if (cart.courses.length) {
+            const html = cart.courses.map(c => {
+              return `
+              <tr>
+              <td>${c.title}</td>
+              <td>${c.count}</td>
+              <td><button class="btn btn-small cyan darken-1 js-remove"
+                  data-id="${c.id}">Delete</button>
+              </td>
+            </tr>`
+            }).join('');
+            $cart.querySelector('tbody').innerHTML = html;
+            $cart.querySelector('.price').textContent = toCurrency(cart.price);
+          } else {
+            $cart.innerHTML = '<p>Cart is empty</p>'
+          }
         })
     }
   })
