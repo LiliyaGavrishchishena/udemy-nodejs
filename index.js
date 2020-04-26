e027ff92aa6e70f88d8d648e0f5ba541155827db
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-04-26 22:51:02 +0300

    connection to MongoDB+mongoose

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path')
+const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
-})
+  extname: 'hbs',
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 //Routes
-app.use('/', require('./routes/home'))
-app.use('/courses', require('./routes/courses'))
-app.use('/add', require('./routes/add'))
-app.use('/cart', require('./routes/cart'))
+app.use('/', require('./routes/home'));
+app.use('/courses', require('./routes/courses'));
+app.use('/add', require('./routes/add'));
+app.use('/cart', require('./routes/cart'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+async function start() {
+  try {
+    const url =
+      'mongodb+srv://liliya:liliya123456@udemy-nodejs-qbdll.azure.mongodb.net/test?retryWrites=true&w=majority';
+
+    await mongoose.connect(url, {
+      useNewUrlParser: true
+    })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+start();
